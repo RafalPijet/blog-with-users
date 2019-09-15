9afa3b279a981f5abd2246c3f4a6a50a98d63f8d
Rafal Pijet <stronglopez@wp.pl>
2019-09-15 17:47:24 +0200

    Add little correct in routing handling

--- client/src/App.js
+++ client/src/App.js
@@ -7,22 +7,39 @@ import Contact from './components/pages/Contact/Contact';
 import PageNotFound from './components/pages/PageNotFound/PageNotFound';
 import Login from './components/pages/Login/Login';
 import Registration from './components/pages/Registration/Registration';
+import Welcome from './components/pages/Welcome/Welcome';
 
 class App extends React.Component {
+    state = {
+        isLogin: false
+    };
 
     render() {
-        return (
-            <MainLayout>
-                <Switch>
-                    <Route path="/" exact component={Home}/>
-                    <Route path="/posts" exact component={Posts}/>
-                    <Route path="/contact" exact component={Contact}/>
-                    <Route path="/login" exact component={Login}/>
-                    <Route path="/registration" exact component={Registration}/>
-                    <Route component={PageNotFound}/>
-                </Switch>
-            </MainLayout>
-        );
+        let {isLogin} = this.state;
+
+        if (isLogin) {
+            return (
+                <MainLayout>
+                    <Switch>
+                        <Route path="/" exact component={Welcome}/>
+                        <Route path="/login" exact component={Login}/>
+                        <Route path="/registration" exact component={Registration}/>
+                        <Route component={PageNotFound}/>
+                    </Switch>
+                </MainLayout>
+            )
+        } else {
+            return (
+                <MainLayout>
+                    <Switch>
+                        <Route path="/" exact component={Home}/>
+                        <Route path="/posts" exact component={Posts}/>
+                        <Route path="/contact" exact component={Contact}/>
+                        <Route component={PageNotFound}/>
+                    </Switch>
+                </MainLayout>
+            );
+        }
     }
 }
 
